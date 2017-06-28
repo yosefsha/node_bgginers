0e2100809a3ba4eff6d800cd037b091e7fc6f7c1
Yossi Shachnovsky <yossi@smartbus.co.il>
2017-06-28 13:08:47 +0300

    dasdas

--- request_handlers.js
+++ request_handlers.js
@@ -11,9 +11,9 @@ var body = '<html>'+
 'charset=UTF-8" />'+
 '</head>'+
 '<body>'+
-'<form action="/upload" method="post">'+
-'<textarea name="text" rows="20" cols="60"></textarea>'+
-'<input type="submit" value="Submit text" />'+
+'<form action="/upload" enctype="multipart/form-data" method="post" >'+
+'<input type="file" name="upload" />'+
+'<input type="Submit" value="Upload file"/>'+
 '</form>'+
 '</body>'+
 '</html>';
